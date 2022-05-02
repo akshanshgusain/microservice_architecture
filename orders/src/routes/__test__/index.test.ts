719dd945412a96cc8063f2ba30e60de62d56ad2e
akshanshgusain <akshanshgusain@gmail.com>
2022-05-02 07:50:40 +0530

    feat(orders): base path and realted test cases

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,55 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Order } from "../../models/orders";
+import { Ticket } from "../../models/ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+  return ticket;
+};
+
+it("fetches orders for a particular user", async () => {
+  // create three tickets
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+  //create one order as user 1
+  const { body: orderOne } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+  //create two orders as user 2
+  const { body: orderTwo } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  //make request to get orders for user two
+  const response = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .expect(200);
+
+  //make sure we only got the order for user 2
+  expect(response.body.length).toEqual(2);
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
+  expect(response.body[0].ticket.id).toEqual(ticketTwo.id);
+  expect(response.body[1].ticket.id).toEqual(ticketThree.id);
+});
--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { requireAuth } from "@sgtickets/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { Order } from "../models/orders";
 
 const router = express.Router();
 
@@ -9,7 +10,10 @@ router.get(
   requireAuth,
   [body("ticketId").not().isEmpty().withMessage("TickerId must be provided")],
   async (req: Request, res: Response) => {
-    res.send({});
+    const orders = await Order.find({
+      userId: req.currentUser!.id,
+    }).populate("ticket");
+    res.send(orders);
   }
 );
 
