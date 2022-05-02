0d9166e5aa9af8d0ee5d2f7183506b1893494f55
akshanshgusain <akshanshgusain@gmail.com>
2022-05-02 09:02:24 +0530

    feat(show): added order show and related tests

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@sgtickets/common";
+import { Order } from "../models/orders";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
