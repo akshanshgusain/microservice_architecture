92bcdcbda2587d426ff5951b8495fbd372c13228
akshanshgusain <akshanshgusain@gmail.com>
2022-05-08 12:10:15 +0530

    feat: completed payments service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,7 +4,7 @@ import { natsWrapper } from "./nats-wrapper";
 import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
 import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
-
+import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 const start = async () => {
   // Check for Environment Variables
   if (!process.env.JWT_KEY) {
@@ -51,7 +51,7 @@ const start = async () => {
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
     new ExpirationCompleteListener(natsWrapper.client).listen();
-
+    new PaymentCreatedListener(natsWrapper.client).listen();
     ///
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,9 @@ import {
 } from "@sgtickets/common";
 import { Order } from "../models/order";
 import { stripe } from "../stripe";
+import { Payment } from "../models/payment";
+import { PaymentCreatedPublisher } from "../events/publishers/payments-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -33,13 +36,25 @@ router.post(
       throw new BadRequestError("Cannot pay for an cancelled order");
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: "usd",
       amount: order.price * 100,
       source: token,
     });
 
-    res.send({ success: true });
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+
+    await payment.save();
+    await new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
+    res.send({ id: payment.id });
   }
 );
 
