933831d5de641ee1bf333eae623b8b0b177d4ce3
akshanshgusain <akshanshgusain@gmail.com>
2022-05-08 09:18:11 +0530

    feat: added payments

---
+++ payments/src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@sgtickets/common";
+
+const app = express();
+
+// Settings
+
+app.set("trust proxy", true);
+
+// Middlewares
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test", // When env is test secure cookie will be false
+  })
+);
+app.use(currentUser);
+
+// Routes
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+// Error handling Middleware
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/index.ts
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  // Check for Environment Variables
+  if (!process.env.JWT_KEY) {
+    throw new Error("Tickets: JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("Tickets: MONGO_URI must be defined");
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("Tickets: NATS_CLIENT_ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("Tickets: NATS_URL must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("Tickets: NATS_CLUSTER_ID must be defined");
+  }
+  console.log("Env variables found.");
+
+  // MongoDB Connection
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    // Shutdown NATS Client
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => {
+      natsWrapper.client.close();
+    });
+    process.on("SIGTERM", () => {
+      natsWrapper.client.close();
+    });
+
+    ///
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Tickets: Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Tickets: Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
